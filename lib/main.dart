737cc981c0d366dbdf0e68d7079093a23239f37e
seversoftech <developersever@gmail.com>
2024-03-20 17:39:25 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,13 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Food Delivery',
-      theme: ThemeData(
-   
-      ),
-      home: LoginPage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Food Delivery',
+        theme: ThemeData(),
+        home: LoginPage());
   }
 }
-
