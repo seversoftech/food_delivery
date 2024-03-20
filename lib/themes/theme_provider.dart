8fce540e9e6e791da2fa832dfb7d1525a5b65185
seversoftech <developersever@gmail.com>
2024-03-20 17:32:20 +0100

    ...

---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/themes/light_mode.dart';
+
+import 'dark_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+}
