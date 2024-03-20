751e6f852dec5dcd10e01ae2cbd61019675816fa
seversoftech <developersever@gmail.com>
2024-03-20 17:25:37 +0100

    ...

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-ThemeData lightMode = ThemeData(
-    colorScheme: ColorScheme.light(
-  background: Colors.grey.shade300,
-  primary: Colors.grey.shade500,
-  secondary: Colors.grey.shade100,
-  tertiary: Colors.white,
-  inversePrimary: Colors.grey.shade700,
-));
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: const Color.fromARGB(255, 20, 20, 20),
+    primary: const Color.fromARGB(255, 122, 122, 122),
+    secondary: const Color.fromARGB(255, 30, 30, 30),
+    tertiary: const Color.fromARGB(255, 47, 47, 47),
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
