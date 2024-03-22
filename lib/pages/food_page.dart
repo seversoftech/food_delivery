7160f7f35771ea881a6afe84cb0d6d91d7f23ec5
seversoftech <developersever@gmail.com>
2024-03-22 08:07:55 +0100

    ...

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -18,27 +18,59 @@ class _FoodPageState extends State<FoodPage> {
       body: Column(
         children: [
           Image.asset(widget.food.imagePath),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(widget.food.name),
-              Text(widget.food.description),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: widget.food.availableAddons.length,
-                itemBuilder: (context, index) {
-                  Addon addon = widget.food.availableAddons[index];
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.food.name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(
+                  widget.food.price.toString(),
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+                SizedBox(height: 10),
+                Text(widget.food.description),
+                Divider(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Add-ons',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                          color: Theme.of(context).colorScheme.secondary)),
+                  child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: widget.food.availableAddons.length,
+                    itemBuilder: (context, index) {
+                      Addon addon = widget.food.availableAddons[index];
 
-                  return CheckboxListTile(
-                    title: Text(addon.name),
-                    subtitle: Text(addon.price.toString()),
-                    value: false,
-                    onChanged: (value) {},
-                  );
-                },
-              ),
-            ],
+                      return CheckboxListTile(
+                        title: Text(addon.name),
+                        subtitle: Text(addon.price.toString()),
+                        value: false,
+                        onChanged: (value) {},
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
