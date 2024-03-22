b2b91622d13a7581179f4955d055874f0db5f226
seversoftech <developersever@gmail.com>
2024-03-22 03:02:21 +0100

    ...

--- lib/components/food_tile.dart
+++ lib/components/food_tile.dart
@@ -38,14 +38,23 @@ class FoodTile extends StatelessWidget {
                     ],
                   ),
                 ),
-                Image.asset(
-                  food.imagePath,
-                  height: 120,
+                SizedBox(width: 15),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    food.imagePath,
+                    height: 120,
+                  ),
                 )
               ],
             ),
           ),
         ),
+        Divider(
+          color: Theme.of(context).colorScheme.tertiary,
+          endIndent: 25,
+          indent: 25,
+        )
       ],
     );
   }
