0b465da249b139b37a1ce04b6628cb9d95a29515
seversoftech <developersever@gmail.com>
2024-03-23 21:08:43 +0100

    ...

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -22,6 +22,7 @@ class DeliveryProgressPage extends StatelessWidget {
 
   Widget _buildBottomNavBar(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(25),
       height: 100,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.secondary,
@@ -41,6 +42,53 @@ class DeliveryProgressPage extends StatelessWidget {
               icon: Icon(Icons.person),
             ),
           ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Sever Manuelz",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              Text(
+                "Driver",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.background,
+                    shape: BoxShape.circle),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.background,
+                    shape: BoxShape.circle),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.call, color: Colors.green),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
