c4aaddfc9070c956c9e6a5dd761e227979eaed1b
seversoftech <developersever@gmail.com>
2024-03-25 12:51:55 +0100

    ...

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/components/receipt.dart';
 
-class DeliveryProgressPage extends StatelessWidget {
+class DeliveryProgressPage extends StatefulWidget {
   const DeliveryProgressPage({super.key});
 
+  @override
+  State<DeliveryProgressPage> createState() => _DeliveryProgressPageState();
+}
+
+class _DeliveryProgressPageState extends State<DeliveryProgressPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
