5efe03620d0813436501fa9f73725b29352eeabd
seversoftech <developersever@gmail.com>
2024-04-03 23:41:38 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food Delivery',
+      title: 'Odogwu Restaurant',
       theme: Provider.of<ThemeProvider>(context).themeData,
       home: AuthGate(),
     );
